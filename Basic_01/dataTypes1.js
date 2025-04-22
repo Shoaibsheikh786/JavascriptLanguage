8a981f7241cc1b16cef7248bb3d4d7505fe03076
Shoaib <50325340+Shoaibsheikh786@users.noreply.github.com>
2025-04-22 06:52:18 +0000

    day1_let_var_const

---
+++ Basic_01/dataTypes1.js
@@ -0,0 +1,21 @@
+const id="myid"
+let n1="shoaib"
+var v1=786;
+myaccount="savingAccount"
+
+console.log(id);
+// id="sheikh"  --> const can't be changed
+n1="sheikh";
+n1=1234
+myaccount="CurrentAccount";
+console.log(id);
+console.table([n1,id,v1,myaccount])
+
+//const can be changed
+//let var , scope difference 
+//without any datatype assigning any value is not good practice
+
+
+
+
+
