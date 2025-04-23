0dc7a239a694bd9e900bb4f7ba3d450e2aa5e8fe
Muneer Ahmad <shoaib786asme@gmail.com>
2025-04-23 12:26:06 +0530

    Opertors in js

---
+++ Basic_01/Operators1.js
@@ -0,0 +1,17 @@
+let num1=10;
+let num2=20
+
+let c=num1+num2;
+
+console.log(c);
+
+console.log(10+"10")
+console.log(10**2);
+console.log(10*2)
+console.log(10%3)
+console.log(10/3)
+console.log(-true)
+
+let abc=Boolean(!true);
+console.log(abc);
+
